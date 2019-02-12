2c71144cc1cd22bb3c5ae788a908a205c0247fc7
Joel Knutson <knutsonj1@csp.edu>
2019-02-12 12:43:49 -0600

    Created new user class Added GET request functionality Data from GET request is parsed and assigned to local variables local variables are being used to create a new user object

--- UserSystem/src/application/MainController.java
+++ UserSystem/src/application/MainController.java
@@ -1,5 +1,10 @@
 package application; // our application package
 
+import application.user;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javafx.event.ActionEvent; // This is our ActionEvent class.
 
 import javafx.fxml.FXML; // This is our FXML class.
@@ -28,8 +33,60 @@ public class MainController { // Our MainController class.
 	private TextField PasswordTextField; // Our PasswordTextField
 	
 	public void Login(ActionEvent e) throws Exception {
-		
-		if(UserNameTextField.getText().equals("username") && PasswordTextField.getText().equals("password")) { // I just used username and password as and example of how a user can login to our system
+            
+            String inputFromURL = null;
+            String username = UserNameTextField.getText();
+            String password = PasswordTextField.getText();
+            String fName="";
+            String lName="";
+            boolean authenticated = false;
+            int startPt=0;
+            int endPt=0;
+            int commaPt=0;
+            
+            //Reference to Sending GET Request and returning data
+            //https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+            //Example GET Request URL
+            //http://csc450.joelknutson.net/authenticate.php?un=jknutson&authpw=12345
+            String url = "http://csc450.joelknutson.net/authenticate.php?un="+username+"&authpw="+password;
+            URL obj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            while((inputFromURL = in.readLine())!=null){
+            response.append(inputFromURL);
+            }
+            in.close();
+            
+            //Debugging Line - server response text (this is the string that is parsed
+            //System.out.println("Server Response: "+response.toString());
+            
+            inputFromURL=response.toString();
+            startPt=inputFromURL.indexOf("<body>")+6;
+            endPt=inputFromURL.indexOf("</body>");
+            commaPt=inputFromURL.indexOf(",", startPt);
+            
+            //Debugging Line - output the location of the comma or -1 if not found
+            //System.out.println("Comma Point: "+commaPt);
+            
+            //Parse the string returned by the server as long as a comma it found
+            if(commaPt != -1){
+            authenticated=true;
+            fName=inputFromURL.substring(startPt, commaPt);
+            lName=inputFromURL.substring(commaPt+1, endPt);
+            
+            //Create user object
+            user loggedInUser = new user(fName,lName);
+            
+            //Debugging Line - output object variables to prove object creation and variable assignment
+            System.out.println("Hello "+loggedInUser.getfName()+" "+loggedInUser.getlName());
+            }
+            else{
+                System.out.println("Authentication Failed");
+            }
+            
+            
+		if(authenticated) { 
 			
 			lblAuthentication.setText("Successful"); // Displays to user that login was successful
 			
